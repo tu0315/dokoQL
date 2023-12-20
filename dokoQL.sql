a9ccd5d090ca97860d504d6f83180384699eb43c
tu0315 <takurou.usuki@gmail.com>
2023-12-20 11:10:28 +0900

    3章まで

--- dokoQL.sql
+++ dokoQL.sql
@@ -25,3 +25,44 @@ where 日付 = '2022-02-10'
 
 DELETE FROM 家計簿
 WHERE 出金額 > 0
+
+SELECT *
+FROM 家計簿
+WHERE 出金額 IS NULL
+
+SELECT *
+FROM 家計簿
+WHERE メモ LIKE '%1月%'
+ 
+select *
+from 家計簿 
+where メモ like '%電気代%'
+
+select *
+from 家計簿
+where 出金額 between 100 and 3000
+
+select *
+from 家計簿
+where 出金額 between 2000 and 5000
+
+select *
+from 家計簿
+where 費目 in ('食費', '交際費', '給料')
+
+select *
+from 家計簿
+where 費目 not in ('食費', '交際費', '給料')
+
+UPDATE 湊くんの買い物リスト
+SET 価格 = 10000
+WHERE 名称 = 'スッキリ勇者クエスト'
+AND 販売店 = 'Y'
+
+-- orをandより優先する
+select *
+from 湊くんの買い物リスト
+where (販売店 = 'A'
+or 販売店 = 'B')
+AND (カテゴリ = 'ゲーム'
+or カテゴリ = 'DVD')
